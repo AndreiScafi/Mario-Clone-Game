b31774f629826ae0f37bdcf54e0c033b03911f65
Andrei Scafi <andreiscafi@hotmail.com>
2023-09-11 18:37:40 -0300

    Adding Gravity to Player.

--- script.js
+++ script.js
@@ -7,12 +7,22 @@ canvas.height = innerHeight;
 
 /* End of Canvas */
 
+/* Gravity */
+
+const gravity = .5;
+
+/* End of Gravity */
+
 /* Player */
 class Player {
     constructor() {
         this.position = {
             x: 100,
             y: 100
+        }
+        this.velocity = {
+            x: 0,
+            y: 0
         }
         this.width = 30;
         this.height = 30;
@@ -22,9 +32,32 @@ class Player {
         c.fillStyle = 'red';
         c.fillRect(this.position.x, this.position.y, this.width, this.height);
     }
+
+    update() {
+        this.draw();
+        this.position.y += this.velocity.y;
+        if (this.position.y + this.height + this.velocity.y <= canvas.height) {
+            this.velocity.y += gravity;
+        } else {
+            this.velocity.y = 0;
+        }
+
+    }
 }
 
 const player = new Player();
-player.draw();
 
 /* End of Player */
+
+/* Animation Function */
+
+function animate() {
+    requestAnimationFrame(animate);
+    c.clearRect(0, 0, canvas.width, canvas.height);
+    player.update();
+}
+
+animate();
+
+/* End of Animation Function */
+
