fa69bca5c7bcd09b73bae832afe9bcc98a8b1000
Andrei Scafi <andreiscafi@hotmail.com>
2023-09-14 11:14:06 -0300

    Adding Player movement with Javascript.

--- script.js
+++ script.js
@@ -36,16 +36,24 @@ class Player {
     update() {
         this.draw();
         this.position.y += this.velocity.y;
+        this.position.x += this.velocity.x;
         if (this.position.y + this.height + this.velocity.y <= canvas.height) {
             this.velocity.y += gravity;
         } else {
             this.velocity.y = 0;
         }
-
     }
 }
 
 const player = new Player();
+const keys = {
+    right: {
+        pressed: false
+    },
+    left: {
+        pressed: false
+    }
+}
 
 /* End of Player */
 
@@ -55,9 +63,112 @@ function animate() {
     requestAnimationFrame(animate);
     c.clearRect(0, 0, canvas.width, canvas.height);
     player.update();
+
+    if (keys.right.pressed) {
+        player.velocity.x = 5
+    } else if (keys.left.pressed) {
+        player.velocity.x = -5
+    } else player.velocity.x = 0;
 }
 
 animate();
 
 /* End of Animation Function */
+
+/* Player Movement */
+
+addEventListener('keydown', ({ keyCode }) => {
+    /*  console.log(keyCode); */
+
+    switch (keyCode) {
+        case 65:
+            console.log('left');
+            keys.left.pressed = true;
+            break;
+        case 37:
+            console.log('left');
+            keys.left.pressed = true;
+            break;
+
+        case 68:
+            console.log('right');
+            keys.right.pressed = true;
+            break;
+        case 39:
+            console.log('right');
+            keys.right.pressed = true;
+            break;
+
+        case 87:
+            console.log('up');
+            player.velocity.y -= 20;
+            break;
+        case 38:
+            console.log('up');
+            player.velocity.y -= 20;
+            break;
+
+        case 83:
+            console.log('down');
+            break;
+        case 40:
+            console.log('down');
+            break;
+
+
+        case 32:
+            console.log('jump');
+            break;
+    }
+    console.log(keys.right.pressed)
+})
+
+addEventListener('keyup', ({ keyCode }) => {
+    console.log(keyCode);
+
+    switch (keyCode) {
+        case 65:
+            console.log('left');
+            keys.left.pressed = false;
+            break;
+        case 37:
+            console.log('left');
+            keys.left.pressed = false;
+            break;
+
+        case 68:
+            console.log('right');
+            keys.right.pressed = false;
+            break;
+        case 39:
+            console.log('right');
+            keys.right.pressed = false;
+            break;
+
+        case 87:
+            console.log('up');
+            player.velocity.y = 0;
+            break;
+        case 38:
+            console.log('up');
+            player.velocity.y = 0;
+            break;
+
+        case 83:
+            console.log('down');
+            break;
+        case 40:
+            console.log('down');
+            break;
+
+
+        case 32:
+            console.log('jump');
+            break;
+    }
+    console.log(keys.right.pressed)
+})
+
+
+/* End of Player Movement */
 
